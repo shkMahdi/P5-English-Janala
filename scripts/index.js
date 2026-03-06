9dcd76899963d6706ba637583f5402409258fab6
Shekh Mahdi Mesbah <mahdisheikh1624@gmail.com>
2026-03-06 18:21:30 +0600

    level-container added

---
+++ scripts/index.js
@@ -0,0 +1,26 @@
+const loadLessons = () => {
+    fetch("https://openapi.programming-hero.com/api/levels/all")
+    .then(res => res.json())
+    .then(json => displayLessons(json.data))
+}
+
+const displayLessons = (lessons) => {
+    // 1. get the container & empty
+    const levelContainer = document.getElementById("level-container");
+    levelContainer.innerHTML = "";
+    // 2. get into every lessons
+    for(let lesson of lessons){
+        // 3.create Element
+        const btnDiv = document.createElement("div");
+        btnDiv.innerHTML = `
+            <button class="btn btn-outline btn-primary">
+                <i class="fa-solid fa-book-open"></i>Lesson - ${lesson.level_no}
+            </button>
+        `;
+        // 4. append into container
+        levelContainer.appendChild(btnDiv);
+    }
+}
+
+loadLessons();
+
